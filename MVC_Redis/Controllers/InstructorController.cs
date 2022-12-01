c5d12154066c9e921c26cf8202baf1b346ef82a6
serhataslan93@gmail.com <serhataslan93@gmail.com>
2022-12-01 23:22:28 +0300

    Öğretmenler için profil sayfası oluşturuldu.Instructor/Profile result'una.

---
+++ MVC_Redis/Controllers/InstructorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Redis.Controllers
+{
+    public class InstructorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        public IActionResult LoadPartial(string view)
+        {
+           
+            return PartialView("_"+view);
+        }
+    }
+}
